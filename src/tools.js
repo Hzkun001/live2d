b5d66a0ce4bcfad7d5f5ecab9b335ae99e097259
Hzkun001 <ratihaura61@gmail.com>
2024-12-03 14:56:45 +0800

    initial commit

---
+++ src/tools.js
@@ -0,0 +1,48 @@
+import fa_comment from "@fortawesome/fontawesome-free/svgs/solid/comment.svg";
+import fa_paper_plane from "@fortawesome/fontawesome-free/svgs/solid/paper-plane.svg";
+import fa_user_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-user.svg";
+import fa_street_view from "@fortawesome/fontawesome-free/svgs/solid/street-view.svg";
+import fa_camera_retro from "@fortawesome/fontawesome-free/svgs/solid/camera-retro.svg";
+import fa_info_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-info.svg";
+import fa_xmark from "@fortawesome/fontawesome-free/svgs/solid/xmark.svg";
+
+import showMessage from "./message.js";
+
+
+const tools = {
+    "asteroids": {
+        icon: fa_paper_plane,
+        callback: () => {
+            if (window.Asteroids) {
+                if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
+                window.ASTEROIDSPLAYERS.push(new Asteroids());
+            } else {
+                const script = document.createElement("script");
+                script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+                document.head.appendChild(script);
+            }
+        }
+    },
+    "switch-model": {
+        icon: fa_user_circle,
+        callback: () => {}
+    },
+    "switch-texture": {
+        icon: fa_street_view,
+        callback: () => {}
+    },
+    "quit": {
+        icon: fa_xmark,
+        callback: () => {
+            localStorage.setItem("waifu-display", Date.now());
+            showMessage("Baka Yaroo!", 2000, 11);
+            document.getElementById("waifu").style.bottom = "-500px";
+            setTimeout(() => {
+                document.getElementById("waifu").style.display = "none";
+                document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
+            }, 3000);
+        }
+    }
+};
+
+export default tools;
